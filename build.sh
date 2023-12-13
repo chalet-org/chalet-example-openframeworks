587396a055204e28053ae311980075696374e450
Andrew King <pepsiotaku@gmail.com>
2023-12-12 21:54:33 -0500

    build.sh - keep box paths the same

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ chalet --distribution-dir dist/Debug -c Debug bundle
 chalet --distribution-dir dist/Release -c Release bundle
 
 cd "$cwd/box"
-chalet --output-dir ../build --distribution-dir ../dist -c Debug build
-chalet --output-dir ../build --distribution-dir ../dist -c Release bundle
+chalet -c Debug build
+chalet -c Release bundle
